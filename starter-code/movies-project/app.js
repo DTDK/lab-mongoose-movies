c4c6e1f3cd97b70694aae7c34388769c83d5bf59
andrezero <andre.torgal@gmail.com>
2018-02-02 13:55:44 +0100

    cleanup app.js

--- starter-code/movies-project/app.js
+++ starter-code/movies-project/app.js
@@ -11,44 +11,47 @@ const index = require('./routes/index');
 const users = require('./routes/users');
 const celebrities = require('./routes/celebrities');
 
+const app = express();
+
+// -- database
+
 mongoose.connect('mongodb://localhost/mongoose-movies-development', {
   keepAlive: true,
   reconnectTries: Number.MAX_VALUE
 });
 
-const app = express();
+// -- view engine
 
-// view engine setup
 app.use(expressLayouts);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.set('layout', 'layouts/main');
-app.use(express.static(__dirname + '/public'));
 
-// uncomment after placing your favicon in /public
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// -- middlewares
+
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// -- routes
+
 app.use('/', index);
 app.use('/users', users);
 app.use('/celebrities', celebrities);
 
-// NOTE: requires a views/not-found.ejs template
+// -- error handlers
+
 app.use(function (req, res, next) {
   res.status(404);
   res.render('not-found');
 });
 
-// NOTE: requires a views/error.ejs template
 app.use(function (err, req, res, next) {
-  // always log the error
   console.error('ERROR', req.method, req.path, err);
-
-  // only render if the error ocurred before sending the response
   if (!res.headersSent) {
     res.status(500);
     res.render('error');
